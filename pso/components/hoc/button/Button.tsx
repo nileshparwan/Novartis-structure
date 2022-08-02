541bbda651dcae7ec4a5b0ff797034207cd71a9f
Koshal Parwan <koshal.parwan@hangarww.com>
2022-08-02 11:34:24 +0400

    [feat] Add prettier, eslint, absolute path & create next app lint app

---
+++ pso/components/hoc/button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, ButtonProps } from '@components/Button';
+
+
+interface ButtonPropsHoc extends ButtonProps {
+  primary?: boolean;
+}
+
+const ButtonWrapper: React.FunctionComponent<ButtonPropsHoc> = (props) => {
+  return (
+    <Button {...props} />
+  );
+}
+
+export default ButtonWrapper; 
