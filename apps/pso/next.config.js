ae912e27b65a2a3cfa7187c27e55dac9e9a9a2aa
Koshal Parwan <parwan.kp@pg.com>
2022-09-10 13:23:15 +0400

    [fix] Error: TypeScript emitted no output

--- apps/pso/next.config.js
+++ apps/pso/next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
 const config = require('./config/config.json');
 
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// })
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -34,7 +35,7 @@ const nextConfig = {
       ...config.optimization,
       minimize: true,
       minimizer: [new TerserPlugin()],
-    }
+    };
 
     config.module.rules.push(
       {
@@ -42,11 +43,16 @@ const nextConfig = {
         exclude: /node_modules/,
         loader: 'graphql-tag/loader',
       },
-    );
+      {
+        test: /\.(tsx|ts)?$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, '../../components'),
+        loader: 'ts-loader',
+      }
+      );
 
     return config;
   },
 };
 
-module.exports = nextConfig;
-// withBundleAnalyzer();
+module.exports = withBundleAnalyzer(nextConfig);
